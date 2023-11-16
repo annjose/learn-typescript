2e5dfba923b442a0613b397c93793bda01a43bb5
Ann Catherine Jose <ann.jose@gmail.com>
2023-11-16 10:08:09 -0800

    Literal types - examples and coding challenges

--- ExecProgram-TypeScriptBasics/06-union-types-literal-types.ts
+++ ExecProgram-TypeScriptBasics/06-union-types-literal-types.ts
@@ -41,9 +41,9 @@ function userNames(u: UserAnother[] | UserAnother): string[] {
 console.log(userNames({ name: 'Amir' }));       // ['Amir']
 console.log(userNames([{ name: 'Amir' }, { name: 'Betty' }]));  // ['Amir', 'Betty']
 // console.log(userNames({ email: 'betty@example.com' })); 
-    // error TS2345: Argument of type '{ email: string; }' is not assignable to 
-    // parameter of type 'UserAnother | UserAnother[]'.        
-    // Object literal may only specify known properties, and 'email' does not exist in type 'UserAnother | UserAnother[]'.
+// error TS2345: Argument of type '{ email: string; }' is not assignable to 
+// parameter of type 'UserAnother | UserAnother[]'.        
+// Object literal may only specify known properties, and 'email' does not exist in type 'UserAnother | UserAnother[]'.
 
 function getZipCode(): number | string {
     return 12345;
@@ -51,3 +51,43 @@ function getZipCode(): number | string {
 // const zip1: number = getZipCode();     // error TS2322: Type 'string | number' is not assignable to type 'number'.
 const zip2: number | string = getZipCode();
 console.log(`zip: ${zip2}`);              // zip: 12345
+
+
+
+// Literal Types
+let one: 1 = 1;
+console.log(one);
+// one = 2;         // error TS2322: Type '2' is not assignable to type '1'.
+// one = "hello";   // error TS2322: Type '"hello"' is not assignable to type '1'
+
+// If you have a variable of type true, you cannot assign a variable of type boolean to it. (boolean variable can hold true or false)
+function giveBoolean() :boolean {
+    return true;
+}
+const b: boolean = giveBoolean(); // OK
+// const t: true = giveBoolean();    // error TS2322: Type 'boolean' is not assignable to type 'true'.
+
+let oneOrTwo: 1 | 2 = 1;
+console.log(`oneOreTwo: ${oneOrTwo}`);  // oneOreTwo: 1
+oneOrTwo = 2;
+console.log(`oneOreTwo: ${oneOrTwo}`);  // oneOreTwo: 2
+// oneOrTwo = 5;       // error TS2322: Type '5' is not assignable to type '2 | 1'.
+
+// Use literal types to restrict the values allowed in a function
+function sortMe(numbers: number[], direction: 'asc' | 'dsc'): number[] {
+
+    const numbers_sortable = numbers.slice();  // clone to array so that we can sort the copy, not the original
+
+    numbers_sortable.sort();    // note: this sort() converts elements to strings and compares the sequences of UTF-16 code units values.
+    if(direction === 'dsc') {
+        numbers_sortable.reverse();
+    }
+
+    return numbers_sortable;
+}
+
+const arr = [3, 4, 1, 2];
+const sortedAsc = sortMe(arr, 'asc');
+const sortedDsc = sortMe(arr, 'dsc');
+console.log(`sortedAsc: ${sortedAsc}, sortedDsc: ${sortedDsc}`);    // sortedAsc: 1,2,3,4, sortedDsc: 4,3,2,1
+// const wrong = sortMe(arr, 'something'); // error TS2345: Argument of type '"something"' is not assignable to parameter of type '"asc" | "dsc"'.
--- ExecProgram-TypeScriptBasics/build/06-union-types-literal-types.js
+++ ExecProgram-TypeScriptBasics/build/06-union-types-literal-types.js
@@ -50,4 +50,34 @@ function getZipCode() {
 // const zip1: number = getZipCode();     // error TS2322: Type 'string | number' is not assignable to type 'number'.
 var zip2 = getZipCode();
 console.log("zip: ".concat(zip2)); // zip: 12345
+// Literal Types
+var one = 1;
+console.log(one);
+// one = 2;         // error TS2322: Type '2' is not assignable to type '1'.
+// one = "hello";   // error TS2322: Type '"hello"' is not assignable to type '1'
+// If you have a variable of type true, you cannot assign a variable of type boolean to it. (boolean variable can hold true or false)
+function giveBoolean() {
+    return true;
+}
+var b = giveBoolean(); // OK
+// const t: true = giveBoolean();    // error TS2322: Type 'boolean' is not assignable to type 'true'.
+var oneOrTwo = 1;
+console.log("oneOreTwo: ".concat(oneOrTwo)); // oneOreTwo: 1
+oneOrTwo = 2;
+console.log("oneOreTwo: ".concat(oneOrTwo)); // oneOreTwo: 2
+// oneOrTwo = 5;       // error TS2322: Type '5' is not assignable to type '2 | 1'.
+// Use literal types to restrict the values allowed in a function
+function sortMe(numbers, direction) {
+    var numbers_sortable = numbers.slice(); // clone to array so that we can sort the copy, not the original
+    numbers_sortable.sort(); // note: this sort() converts elements to strings and compares the sequences of UTF-16 code units values.
+    if (direction === 'dsc') {
+        numbers_sortable.reverse();
+    }
+    return numbers_sortable;
+}
+var arr = [3, 4, 1, 2];
+var sortedAsc = sortMe(arr, 'asc');
+var sortedDsc = sortMe(arr, 'dsc');
+console.log("sortedAsc: ".concat(sortedAsc, ", sortedDsc: ").concat(sortedDsc)); // sortedAsc: 1,2,3,4, sortedDsc: 4,3,2,1
+// const wrong = sortMe(arr, 'something'); // error TS2345: Argument of type '"something"' is not assignable to parameter of type '"asc" | "dsc"'.
 //# sourceMappingURL=06-union-types-literal-types.js.map
